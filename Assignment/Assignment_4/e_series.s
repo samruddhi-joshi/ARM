d2089fc08bd0c387028dbc554213abf6f5392a1e
samruddhi-joshi <44343518+samruddhi-joshi@users.noreply.github.com>
2018-11-20 23:28:54 +0530

    Create e_series.s

---
+++ Assignment/Assignment_4/e_series.s
@@ -0,0 +1,27 @@
+PRESERVE8
+     THUMB
+     AREA     appcode, CODE, READONLY
+     EXPORT __main
+	 ENTRY 
+__main  FUNCTION
+
+	VMOV.F32 S1, #6  ; y to find e^y
+	VMOV.F32 S2, #10 ; Number of terms in e^y expansion
+	VMOV.F32 S3, #1  ; count
+	VMOV.F32 S4, #1  ; temp
+	VMOV.F32 S5, #1  ; result
+	VMOV.F32 S7, #1  ; register to hold one
+Loop 
+	 VCMP.F32 S2, S3 ; Comparison done for excuting taylor series expansion of e^x for s2 number of terms
+
+	 VMRS.F32 APSR_nzcv,FPSCR ; Used to copy FPSRC to APSR
+	 BLT stop;
+	 VDIV.F32 S6, S1, S3 ; temp1=y/count
+	 VMUL.F32 S4, S4, S6 ; temp=temp*temp1;
+	 VADD.F32 S5, S5, S4 ; result=result+temp;
+	 VADD.F32 S3, S3, S7 ; incrementing count
+	 B Loop; 
+	 
+stop B stop ; stop program
+	 ENDFUNC
+	 END
