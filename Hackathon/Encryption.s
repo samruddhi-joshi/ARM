a93b0c33ecc10d32d25e76a61fe4ce356427e860
samruddhi-joshi <44343518+samruddhi-joshi@users.noreply.github.com>
2018-11-27 13:35:40 +0530

    Update Encryption.s

--- Hackathon/Encryption.s
+++ Hackathon/Encryption.s
@@ -1,22 +1,26 @@
-     AREA     Encryption, CODE, READONLY
-     export __main	 
+         AREA     Encryption, CODE, READONLY
+    	 import printMsg
+	 export __main	 
 	 ENTRY 
 __main  function
+	MOV r2,#0x20000000
+	ADD r2,r2,#4
+	MOV r3,r2
 	MOV r0,r2;    starting address in r2
-	MOV r4,0
-	MOV r3,#0x20000000
+	MOV r4,#0
 Loop
-	LDRB r1,[r0],#4  ; offset=4, post indexed so that value of location keeps incrementing
+
+	LDRB r1,[r0],#1  ; offset=4, post indexed so that value of location keeps incrementing
 	EORS r1,r1,#0x00000024	; r3 i temp
 	EORS r1,r1,#0x00000001	;to negate the LSB
-	STRB r1,[r3],#4			;post indexed
+	STRB r1,[r3],#1		;post indexed
+	MOV r0,r1
+	BL printMsg
 	ADD r4,r4,#1
 	CMP r4,#614400
 	IT LS
 	BLS Loop
 
-STOP B STOP
-	
- 
-endfunc
-END
+STOP    B STOP
+	endfunc
+	end
